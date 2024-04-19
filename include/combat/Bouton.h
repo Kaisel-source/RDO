722a5b5b5d83efc7c9c9b05f2bb99e76096792bd
MAELGAUMONT <123172308+MAELGAUMONT@users.noreply.github.com>
2024-04-19 19:40:07 +0200

    Update Bouton.h

--- include/combat/Bouton.h
+++ include/combat/Bouton.h
@@ -5,14 +5,33 @@
 
 
 
-
-// Initialiser un bouton avec les coordonnées et dimensions spécifiées
+/**
+ * @brief // Initialiser un bouton avec les coordonnées et dimensions spécifiées
+ * 
+ * @param x 
+ * @param y 
+ * @param width 
+ * @param height 
+ * @return Button 
+ */
 Button init_Button(int x, int y, int width, int height);
 
-// Obtenir le rectangle d'un bouton à partir de ses coordonnées
+/**
+ * @brief // Obtenir le rectangle d'un bouton à partir de ses coordonnées
+ * 
+ * @param bouttonx 
+ * @param bouttony 
+ * @return SDL_Rect 
+ */
 SDL_Rect get_Button_Rect(int bouttonx, int bouttony);
 
-// Coller une image sur un bouton
+/**
+ * @brief // Coller une image sur un bouton
+ * 
+ * @param renderer 
+ * @param imageTexture 
+ * @param buttonRect 
+ */
 void set_Button_Image(SDL_Renderer* renderer, SDL_Texture* imageTexture, SDL_Rect* buttonRect);
 
 #endif /* BOUTON_H */
