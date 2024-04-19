66de012f3b313a06d9e5cca7a7766bae9ed356be
MAELGAUMONT <123172308+MAELGAUMONT@users.noreply.github.com>
2024-04-19 19:53:03 +0200

    Update entite.h

--- include/combat/entite.h
+++ include/combat/entite.h
@@ -4,9 +4,27 @@
 #include "../common/common.h"
 
 
-
-// Déclaration des fonctions pour le fichier entite.c
+/*
+ * @brief Create a entity object
+ * 
+ * @param prenom 
+ * @param classe 
+ * @param pv 
+ * @param pv_max 
+ * @param attaque 
+ * @param equipe 
+ * @param mouv 
+ * @param initiative 
+ * @param range 
+ * @return entite_t 
+ */
 entite_t create_entity(const char *prenom, const char *classe, int pv, int pv_max, int attaque, int equipe, int mouv, int initiative, int range);
+
+/**
+ * @brief met les stat a 0 et a NULL
+ * 
+ * @param entite 
+ */
 void initialiser_Entite(entite_t *entite);
 
 #endif /* ENTITE_H */
